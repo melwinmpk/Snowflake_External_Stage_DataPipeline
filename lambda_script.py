1b0fc49440190d59c350c640cec6a6069c7a7eb4
melwinmpk <melwinmpk13@gmail.com>
2024-03-25 19:06:13 +0530

    Modified the Lambda Script

--- lambda_script.py
+++ lambda_script.py
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     SNOWFLAKE_CREDENTIALS = {
                             "USER": "MELWINMPK",
                             "PASSWORD": "",
-                            "ACCOUNT": "bfbuljl-bnb23563",
+                            "ACCOUNT": "",
                             "WHAREHOUSE": "COMPUTE_WH",
                             "DATABASE":"amazonebooks_external",
                             "SCHEMA":"PUBLIC"
@@ -69,50 +69,55 @@ def lambda_handler(event, context):
     curr.execute(snow_query)
         
     # Getting the Primary Key from the config Table
-    snow_query = 'USE SCHEMA CONFIG;'
-    curr.execute(snow_query)
     
     snow_query = f'''SELECT * FROM CONFIG.tbl_primary_key where table_name = '{MAIN_DATA["table_name"]}';'''
     primary_key_data_df = pd.read_sql(snow_query, conn)
     
-    snow_query = f'USE SCHEMA {MAIN_DATA["Schema"]};'
-    curr.execute(snow_query)
-    
     if MAIN_DATA["load_type"] == "Incremental":
+        
+        '''
+        MERGE INTO AMAZONE_BOOKS T1
+          USING (SELECT * FROM AMAZONE_BOOKS_EXT WHERE FILE_DATE_PARTITION > 20240226 ) T2 ON T1.BOOK_ID = T2.BOOK_ID 
+          WHEN MATCHED THEN UPDATE SET T1.BOOK_TITLE = T2.BOOK_TITLE,
+                                        T1.BOOK_AMOUNT = T2.BOOK_AMOUNT,
+                                        T1.BOOK_RATING = T2.BOOK_RATING, 
+                                        T1.BOOK_LINK = T2.BOOK_LINK,
+                                        T1.BUSINESS_DATE = T2.BUSINESS_DATE,
+                                        T1.FILE_DATE_PARTITION = T2.FILE_DATE_PARTITION
+          WHEN NOT MATCHED THEN INSERT (BOOK_ID, BOOK_TITLE, BOOK_AMOUNT, BOOK_AUTHOR, BOOK_RATING, BOOK_LINK, BUSINESS_DATE , FILE_DATE_PARTITION)
+          VALUES (T2.BOOK_ID, T2.BOOK_TITLE, T2.BOOK_AMOUNT, T2.BOOK_AUTHOR, T2.BOOK_RATING, T2.BOOK_LINK, T2.BUSINESS_DATE , T2.FILE_DATE_PARTITION);
+        '''
     
-        query = f'DELETE FROM  {MAIN_DATA["table_name"]} T1 USING ( SELECT '
+        query = f''' MERGE INTO {MAIN_DATA["table_name"]} T1 
+                     USING ( 
+                         SELECT * FROM {MAIN_DATA["External_Table"]} t 
+                         WHERE FILE_DATE_PARTITION > {(str(last_extract_dt_df["LAST_EXTRACT_DATE"][0])).replace("-","")} ) T2 ON '''
         query_part1 = ''
         query_part2 = ''
         query_part3 = ''
-    
+        query_part4 = ''
     
         for i in range(primary_key_data_df.shape[0]):
-            
             primary_column = (primary_key_data_df.loc[i,["PRIMARY_COLUMN"]]).item()
+            query_part1 += f' T1.{primary_column} = T2.{primary_column} and'
     
-            query_part1 += f' {primary_column},'
-            query_part2 += f' T1.{primary_column},'
-            query_part3 += f' T1.{primary_column} = T2.{primary_column} and'
-    
-        query += (query_part1[:-1] + f''' FROM {MAIN_DATA["External_Table"]} t WHERE FILE_DATE_PARTITION > {(str(last_extract_dt_df["LAST_EXTRACT_DATE"][0])).replace("-","")} ) T2 WHERE {query_part3[:-3]};''')
+        query += f'''{query_part1[:-3]} WHEN MATCHED THEN UPDATE SET '''
+        
+        snow_query = f''' SHOW COLUMNS IN TABLE {MAIN_DATA["External_Table"]};  '''
+        table_columns_df = pd.read_sql(snow_query, conn)
+        
+        for column in  table_columns_df["column_name"]: 
+            if column != 'VALUE':
+                query_part2+= f''' T1.{column} = T2.{column},'''
+                query_part3+= f''' {column},'''
+                query_part4+= f''' T2.{column},'''
+
+        query += f'''{query_part2[:-1]} WHEN NOT MATCHED THEN INSERT ({query_part3[:-1]}) VALUES ({query_part4[:-1]});''' 
         curr.execute(query)
         print(query)
     
-    snow_query = f''' SHOW COLUMNS IN TABLE {MAIN_DATA["External_Table"]};  '''
-    table_columns_df = pd.read_sql(snow_query, conn)
     
-    # INSERTING THE NEW DATA TO THE TABLE
-    table_columns_strig =  ','.join([ i for i in table_columns_df["column_name"] if i != 'VALUE' ])
-    
-    snow_query = f''' INSERT INTO {MAIN_DATA["table_name"]} 
-                      (SELECT {table_columns_strig} FROM {MAIN_DATA["External_Table"]} WHERE FILE_DATE_PARTITION > {(str(last_extract_dt_df["LAST_EXTRACT_DATE"][0])).replace("-","")});'''
-                      
-    print(snow_query)
-    
-    curr.execute(snow_query)
-    
-    snow_query = 'USE SCHEMA CONFIG;'
-    curr.execute(snow_query)
+    # UPDATING THE CONFIG TABLE
     
     query = f''' UPDATE CONFIG.TABLE_CONFIG
                  SET LAST_EXTRACT_DATE = (SELECT MAX(business_date) FROM PUBLIC.{MAIN_DATA["table_name"]})
@@ -121,7 +126,6 @@ def lambda_handler(event, context):
     
     curr.execute(query)
     
-    # UPDATING THE CONFIG TABLE
     
     conn.close()
     print("Done")
